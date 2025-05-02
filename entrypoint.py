58cef8999f36163b62f2e2e2aaf74600f2b885c7
Coeptus <jaksei.lol@gmail.com>
2025-05-02 15:14:46 +0200

    Refactor API integration to use environment variable for API URL and improve error handling in entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import time
 import os
 import logging
+from api_service import get_tournaments
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Setup")
@@ -14,6 +16,7 @@ REQUIRED_ENV_DICT ={
     "SMTP_PASSWORD": os.getenv("SMTP_PASSWORD", None),
     "RECIPIENTS": os.getenv("RECIPIENTS", None),
     "NOTIFIED_PATH": os.getenv("NOTIFIED_PATH", None),
+    "API_URL": os.getenv("API_URL", None),
 }
 
 missing_env_vars = [key for key, value in REQUIRED_ENV_DICT.items() if value is None]
@@ -37,9 +40,19 @@ else:
     with open(NOTIFIED_FILE, "w") as f:
         f.write("")
     
-from api_service import get_tournaments
 
-tournaments = get_tournaments()
+logger.info("Waiting for 20 seconds before starting the API service")
+
+time.sleep(20)
+logger.info("Starting API service")
+
+try:
+    tournaments = get_tournaments()
+except Exception as e:
+    logger.error(f"Error getting tournaments: {e}")
+    logger.error("Exiting")
+    exit(1) 
+
 if not tournaments:
     logger.warning("No tournaments found")
 
